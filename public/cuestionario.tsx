93acf963aaeb3eb2679596981be53499786a968d
perita23 <hugobedmar10@gmail.com>
2025-02-24 17:56:47 +0100

    Refactor questionnaire handling by removing cuestionario.json and implementing cuestionario.tsx for dynamic data loading in CuestionarioComponent

---
+++ public/cuestionario.tsx
@@ -0,0 +1,22 @@
+const cuestionario = [
+  {
+    "id": "nombre",
+    "tipo": "text",
+    "pregunta": "¿Cuál es tu nombre?",
+    "respuesta": ""
+  },
+  {
+    "id": "edad",
+    "tipo": "number",
+    "pregunta": "¿Cuántos años tienes?",
+    "respuesta": ""
+  },
+  {
+    "id": "sexo",
+    "tipo": "select",
+    "pregunta": "¿Cuál es tu sexo?",
+    "opciones": ["Masculino", "Femenino", "Otro"],
+    "respuesta": ""
+  }
+];
+export default cuestionario
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import cuestionario from '../public/cuestionario.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App cuestionario={cuestionario} />
   </StrictMode>,
 )
