dd2773d188ebc5248ac10af17fd1b3234e0c629a
perita23 <hugobedmar10@gmail.com>
2025-02-24 18:11:46 +0100

    Refactor styles by removing old CSS files and consolidating styles into new App.css and index.css

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Adiós</h1>
+    </div>
+  );
+}
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/App.css'
 import App from './App.tsx'
-import cuestionario from '../public/cuestionario.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App cuestionario={cuestionario} />
+    <App />
   </StrictMode>,
 )
