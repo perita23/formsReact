e32f5ad15287f34a857c23dd2947d829797b1fbf
Hugo Gonzalez <hugobedmar10@gmail.com>
2025-02-27 19:10:10 +0100

    Update index.html title, add background animation component, and remove old CSS import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/header/navBar/NavBar";
+import Bg from "./components/UI/bg/bg"; // Assuming bg is a background image or style
 import Home from "./pages/homePage/Home";
 import "./styles/App.css";
 function App() {
   return (
     <Router>
+      <Bg />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/components/UI/bg/bg.tsx
@@ -0,0 +1,8 @@
+import "./bg.css";
+const bgAnimation = () => {
+  return (
+    <div className="bg">Hola</div>
+  )
+}
+
+export default bgAnimation;
