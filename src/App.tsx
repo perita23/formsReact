8f580d6a851ab698f808f0b0c35b51054880d305
Hugo Gonzalez <hugobedmar10@gmail.com>
2025-03-19 15:32:45 +0100

    Add HomeSlider component and enhance Home page layout; update styles and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { SurveyPage } from './pages/SurveyPage';
-import './styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { SurveyPage } from "./pages/SurveyPage";
+import "./styles/App.css";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
+        <div className="logo-cont">
+          Quest
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="32px"
+            viewBox="0 -1080 960 960"
+            width="32px"
+            fill="#ffffff"
+          >
+            <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
+          </svg>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/survey" element={<SurveyPage />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import HomeSlider from "../components/HomeSlider";
 
 export const Home = () => {
   return (
     <div className="home">
-      <h1>Bienvenido a las Encuestas</h1>
-      <p>Haz clic para empezar:</p>
-      <Link to="/survey">Comenzar Encuesta</Link>
+      <div className="home-top-row">
+        <h1 className="htr-title">
+          Bienvenido <br></br> a las Encuestas
+        </h1>
+      </div>
+      <div className="home-bot-row">
+        <div className="hbr-cont hbr-1-cont">
+          <p className="hbr-cont-text">
+            Participa en nuestras encuestas y ayuda a mejorar nuestros
+            servicios.
+          </p>
+          <Link className="button" to="/survey">
+            Comenzar
+            <svg
+              className="button-icon custom-fill"
+              xmlns="http://www.w3.org/2000/svg"
+              height="30px"
+              viewBox="0 -960 960 960"
+              width="30px"
+              fill="#FFFFFF"
+            >
+              <path d="M246.67-244 200-290.67l402.67-402.66H236V-760h480v480h-66.67v-366.67L246.67-244Z" />
+            </svg>
+          </Link>
+        </div>
+        <div className="hbr-cont">
+          <HomeSlider/>
+        </div>
+        <div className="hbr-cont">
+          <div className="hbr-cont-img" ></div>
+        </div>
+      </div>
     </div>
   );
 };
